9beae92099b58992cbdb389e2c221d6831af4c7b
Glave <36699022+Glave@users.noreply.github.com>
2019-10-07 15:53:05 +0800

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KPMS.MvcApplication" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using KPMS.App_Start;
+using KPMS.Controllers;
+using System;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace KPMS
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+  
+    }
+}
